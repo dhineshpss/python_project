8ae5e7b17d65adb65cef729887c755cca2b67073
dhineshpss <dhineshselvam92@gmail.com>
2026-06-09 01:35:26 +0530

    test workflow

--- check_cpu.py
+++ check_cpu.py
@@ -5,7 +5,7 @@ def check_cpu_usage():
     cpu_percent = psutil.cpu_percent(interval=1)
     print(f"CPU Usage: {cpu_percent}%")
 
-for i in range(5):  # Check CPU usage 5 times
+for i in range(8):  # Check CPU usage 5 times
     print   ("Checking CPU usage...")
     check_cpu_usage()
     print("-" * 30)
